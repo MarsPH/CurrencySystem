1c388d20ee2c8123474cfcbd21d8f147d8d1e6fa
MarsPH <m.poorhamidian@gmail.com>
2025-09-12 13:39:28 -0400

    AddCurrency function prints the new amount on the screen.

--- Source/CurrencySystem/Private/CurrencyComponent.cpp
+++ Source/CurrencySystem/Private/CurrencyComponent.cpp
@@ -3,8 +3,10 @@
 
 #include "CurrencyComponent.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 // Sets default values for this component's properties
-UCurrencyComponent::UCurrencyComponent()
+UCurrencyComponent::UCurrencyComponent(): CurrentCurrency(0), MaxCurrency(0)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,7 +17,27 @@ UCurrencyComponent::UCurrencyComponent()
 
 void UCurrencyComponent::AddCurrency(int32 Amount)
 {
-	CurrentCurrency += Amount;
+	CurrentCurrency += Amount; 
+
+	TArray<int32> MyCurrencies = {1, 2, 3, 3, 3, 1};
+
+	FString stringResult = TEXT("");
+	for (int i = 0; i < MyCurrencies.Num(); i++)
+	{
+		stringResult = stringResult.Appendf(TEXT("Number_%d: %d, "), i + 1, MyCurrencies[i]);
+	}
+	
+	UKismetSystemLibrary::PrintString(this, stringResult, true, false, FColor::Red, 10, "Currency");
+}
+
+bool UCurrencyComponent::SpendCurrency(int32 Amount)
+{
+	return true;
+}
+
+bool UCurrencyComponent::CanAfford(int32 Amount)
+{
+	return true;
 }
 
 
