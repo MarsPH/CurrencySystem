6d9cf1505c84998b6f66e1494b3d36fcfbbf7bbb
MarsPH <m.poorhamidian@gmail.com>
2025-09-15 20:54:44 -0400

    Added: WidgetComponent for UI Feedback of Costable.cpp

--- Source/CurrencySystem/Private/Costable.cpp
+++ Source/CurrencySystem/Private/Costable.cpp
@@ -12,6 +12,17 @@ ACostable::ACostable()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(FName("BoxComponent")); //Assigning an subobject of box comp
+	BoxComponent->SetBoxExtent(FVector(150.0f, 150.0f, 150.0f));
+
+	WidgetComponent = CreateDefaultSubobject<UWidgetComponent>(FName("WidgetComponent"));
+	WidgetComponent-> SetRelativeLocation(FVector(0, 0, 100));
+	WidgetComponent->Text
+
+	
+
+
 }
 
 int32 ACostable::GetCost_Implementation()
@@ -25,8 +36,39 @@ int32 ACostable::GetCost_Implementation()
 void ACostable::BeginPlay()
 {
 	Super::BeginPlay();
+
+		RootComponent = GetRootComponent();
+		BoxComponent = NewObject<UBoxComponent>(RootComponent); //creates an object and assign to box component
+		if (BoxComponent != nullptr){ //checks box component to be not null
+
+			BoxComponent->RegisterComponent();
+			BoxComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+			BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ACostable::OverlapBegin);// bind begin overlap
+			BoxComponent->OnComponentEndOverlap.AddDynamic(this, &ACostable::OverlapEnd);// bind end overlap
+		}
+
+		WidgetComponent = NewObject<UWidgetComponent>(RootComponent);
+		if (WidgetComponent != nullptr)
+		{
+			WidgetComponent->RegisterComponent();//creates the physical state
+			WidgetComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+			WidgetComponent->SetHiddenInGame(true);//it only is visible under conditions
+		}
+	
 }
 
+void ACostable::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+		WidgetComponent->SetHiddenInGame(false);
+}
+
+void ACostable::OverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	WidgetComponent->SetHiddenInGame(true);
+}
 
 
 // Called every frame
--- Source/CurrencySystem/Public/Costable.h
+++ Source/CurrencySystem/Public/Costable.h
@@ -6,9 +6,12 @@
 #include "ICostable.h"
 #include "GameFramework/Actor.h"
 #include "ICostable.h"
+#include "Components/WidgetComponent.h"
 
 #include "Costable.generated.h"
 class UBoxComponent;
+
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnBoxOverlappedWithPlayer, int32, Cost);
 
 UCLASS()
 class CURRENCYSYSTEM_API ACostable : public AActor, public IICostable
@@ -22,9 +25,24 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Cost;
 
+	
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult); // overlapBegin
+
+	UFUNCTION()
+	void OverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex); // OverlapEnd
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UBoxComponent* BoxComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UWidgetComponent* WidgetComponent;
 
 
 public:	
